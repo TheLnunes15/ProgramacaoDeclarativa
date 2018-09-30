cbf2e85f6b65e75058d4bc3f5d9fa7d26bb4be5b
thelnunes15 <lucas.nunes@dcomp.ufs.br>
2018-09-30 20:21:58 -0300

    deletado

--- rot.hs
+++
@@ -1,7 +0,0 @@
-muda :: [[a]] -> [[a]]
-muda []             = []
-muda ([]   : xs')   = muda xs'
-muda ((x:xs) : xs') = (x : [p | (p:_) <- xs']) : muda (xs : [ q | (_:q) <- xs'])    
-
-rot :: [[Int]] -> [[Int]]
-rot = reverse . muda
